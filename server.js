5097ac995111bb082e74cbc2d8653a9697127661
rickknowlton <richard.d.knowlton@gmail.com>
2018-11-29 13:32:08 -0500

    v 1.0.1

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+
+
+var app = express();
+
+
+app.use(express.static(__dirname + '/app/public'));
+
+var PORT = process.env.PORT || 8080;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
+app.listen(PORT, function () {
+    console.log(`Listening on port: ${PORT}`);
+});
+
+
+
